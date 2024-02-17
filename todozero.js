ffa1afc837bd7c2287760ce842264b55a6179032
hetaredaimaou <hanadekayu@icloud.com>
2024-02-17 20:40:02 +0900

    first commit

---
+++ todozero.js
@@ -0,0 +1,16 @@
+function addTask() {
+    var taskInput = document.getElementById('taskInput');
+    var taskList = document.getElementById('taskList');
+
+    if (taskInput.value !== '') {
+        var li = document.createElement('li');
+        li.textContent = taskInput.value;
+        taskList.appendChild(li);
+        taskInput.value = '';
+
+        // タスクをクリックしたら完了状態にする
+        li.addEventListener('click', function () {
+            li.classList.toggle('completed');
+        });
+    }
+}
